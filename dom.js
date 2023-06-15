a6d52b5f438690a5ef81df66d30600632838a9c6
ishhaxkaur <kaurxishaa@gmail.com>
2023-06-16 00:02:14 +0530

    3rd changes

--- dom.js
+++ dom.js
@@ -1,20 +1,24 @@
 //console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.doctype);
-console.log(documen.all[10]);
-console.log(document.forms);
-console.log(document.links);
-console.log(document.getElementById('header-title'));
- var headerTitle = document.getElementById('header-title');
- console.log(headerTitle);
- headerTitle.textContent= 'hello';
- headerTitle.innerText="goodbye";
- console.log(headerTitle.innerText);
- headerTitle.innerHTML= '<h3>Hello</3';
- headerTitle.style.borderBottomColor - 'solid 3px #000';
-var Add items = document.getElementsByClassName
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.doctype);
+//console.log(documen.all[10]);
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.getElementById('header-title'));
+ //var headerTitle = document.getElementById('header-title');
+ //console.log(headerTitle);
+ //headerTitle.textContent= 'hello';
+ //headerTitle.innerText="goodbye";
+ //console.log(headerTitle.innerText);
+ //headerTitle.innerHTML= '<h3>Hello</3';
+ //headerTitle.style.borderBottomColor - 'solid 3px #000';
+var items = document.getElementsByClassName('list-group-items');
+console.log(items);
+console.log(item[3]);
+items[3].style.bacgroundColor = 'Green'
+items[3].style.fontWeight = 'bold'
