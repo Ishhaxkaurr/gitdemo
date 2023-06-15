f04fe5b97b5d4164a6405669faf3d38e30020267
ishhaxkaur <kaurxishaa@gmail.com>
2023-06-15 23:40:10 +0530

    2nd

---
+++ dom.js
@@ -0,0 +1,20 @@
+//console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.doctype);
+console.log(documen.all[10]);
+console.log(document.forms);
+console.log(document.links);
+console.log(document.getElementById('header-title'));
+ var headerTitle = document.getElementById('header-title');
+ console.log(headerTitle);
+ headerTitle.textContent= 'hello';
+ headerTitle.innerText="goodbye";
+ console.log(headerTitle.innerText);
+ headerTitle.innerHTML= '<h3>Hello</3';
+ headerTitle.style.borderBottomColor - 'solid 3px #000';
+var Add items = document.getElementsByClassName
