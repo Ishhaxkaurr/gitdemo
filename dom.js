1348df89a8946dd6cabe71140c013886bf700838
ishhaxkaur <kaurxishaa@gmail.com>
2023-06-16 00:41:08 +0530

    5th

--- dom.js
+++ dom.js
@@ -17,12 +17,25 @@
  //console.log(headerTitle.innerText);
  //headerTitle.innerHTML= '<h3>Hello</3';
  //headerTitle.style.borderBottomColor - 'solid 3px #000';
-var items = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[2]);
-li[2].style.bacgroundColor = 'red'
-li[2].style.fontWeight = 'Italics'
+//var items = document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[2]);
+//li[2].style.bacgroundColor = 'red'
+//li[2].style.fontWeight = 'Italics'
 
-for(var i = 0; li.length; i++){
-    li[i].style.backgroundColor= 'pink';
+//for(var i = 0; li.length; i++){
+//    li[i].style.backgroundColor= 'pink';
+//}
+//var header = document.querySelector('#main-header');
+//header.style.borderBottom = 'solid 4px #ccc';
+
+//var input = document.querySelector('input')
+//input.value = 'hello world';
+
+var secondItem =  document.querySelector('.list-group-item:nth=-child(2)');
+secondItem.style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i = 0; i<odd.length; i++){
+    odd[i].style.backgroundcolor = 'green'
 }
